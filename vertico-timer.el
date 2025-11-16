dc61e99de921c79f796be8b94720196e2826fe91
Vince Vice <vincent.troetschel@mailbox.org>
2025-11-16 21:20:47 +0100

    [fix] Update gh reference

--- vertico-timer.el
+++ vertico-timer.el
@@ -8,7 +8,7 @@
 ;; Modified: November 03, 2025
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
-;; Homepage: https://github.com/VitruvianVice/vertico-timer.git
+;; Homepage: https://github.com/ventruvian/vertico-timer.git
 ;; Package-Requires: ((emacs "29.1"))
 ;;
 ;; This file is not part of GNU Emacs.
